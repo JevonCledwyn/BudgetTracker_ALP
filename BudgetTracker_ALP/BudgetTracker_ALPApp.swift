bb6d9689f8014267f3e24bbcb7e9801d5acf0024
JevonCledwyn <jcledwyn@student.ciputra.ac.id>
2023-05-28 16:03:38 +0700

    Initial Commit

---
+++ BudgetTracker_ALP/BudgetTracker_ALPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTracker_ALPApp.swift
+//  BudgetTracker_ALP
+//
+//  Created by MacBook Pro on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BudgetTracker_ALPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
